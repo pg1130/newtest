566e5abf980730b3850118b74d01a23cb4f0b2f9
pg1130 <pg1130@hanmail.net>
2023-08-23 14:46:28 +0900

    login 적용

--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -5,6 +5,9 @@ from . import views #views.py import
 router = routers.DefaultRouter() #DefaultRouter를 설정
 router.register('Item', views.ItemViewSet) #itemviewset 과 item이라는 router 등록
 
+
+
 urlpatterns = [
     path('', include(router.urls)) #initial URL
 ]
+
